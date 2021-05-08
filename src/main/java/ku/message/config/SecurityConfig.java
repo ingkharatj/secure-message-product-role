50df40e014f5532c3f51ec0c7c8cfa40931871bd
Ing <ingkharat.j@ku.th>
2021-05-08 22:46:58 +0700

    finish

---
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package ku.message.config;
+
+//import ku.message.service.AuthenticationService;
+import ku.message.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private AuthenticationService authenticationService;
+
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/home", "/signup", "/css/**", "/js/**").permitAll()
+                .antMatchers("/post").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/message").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .defaultSuccessUrl("/message", true)
+                .and().logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(this.authenticationService);
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+
+
+}
